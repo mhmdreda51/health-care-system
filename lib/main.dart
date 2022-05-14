3d607a263a0c0e635200e9e46ebe2d9e2cafdb83
Muhammed Reda <60475330+mhmdreda51@users.noreply.github.com>
2022-05-15 01:13:21 +0200

    Change App icon and App name

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:health_care_system/view/Home%20Screen/Controller/home_cubit.dart';
 import 'package:health_care_system/view/Splash%20Screen/splash_screen.dart';
-
+art';
 import 'core/blocObserver/bloc_observer.dart';
 import 'core/cacheHelper/cache_helper.dart';
 import 'core/dioHelper/dio_helper.dart';
@@ -12,16 +12,17 @@ import 'core/router/router.dart';
 import 'core/theme/theme.dart';
 
 void main() async {
+  //===============================================================
   WidgetsFlutterBinding.ensureInitialized();
+  //===============================================================
   await Firebase.initializeApp();
   //===============================================================
   Bloc.observer = MyBlocObserver();
+  //===============================================================
   DioHelper.init();
   //===============================================================
   await CacheHelper.init();
   //===============================================================
-  //===============================================================
-
   runApp(
     const MyApp(),
   );
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
       ],
       child: ScreenUtilInit(
         designSize: const Size(375, 812),
-        builder: () => MaterialApp(
+        builder: (_) => MaterialApp(
           title: 'Health Care System',
           debugShowCheckedModeBanner: false,
           navigatorKey: navigatorKey,
